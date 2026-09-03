7b1d10cb90db736df4bda9abef8620273c146ee6
Matt Hinz <matthinz@gmail.com>
2026-09-03 15:24:39 -0700

    Initial Commit

---
+++ kid-video-thing/kid_video_thingApp.swift
@@ -0,0 +1,17 @@
+//
+//  kid_video_thingApp.swift
+//  kid-video-thing
+//
+//  Created by Matt Hinz on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct kid_video_thingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
